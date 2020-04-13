9c4830502583dec03a3e1ebdc399689e68b04967
Stefania Popescu <38551766+stefaniaPopescu26@users.noreply.github.com>
2020-04-13 12:10:08 +0300

    Exercitiul B2

--- Colocviul_13/app/src/main/java/ro/pub/cs/systems/eim/Colocviul_13/Colocviul_13MainActivity.java
+++ Colocviul_13/app/src/main/java/ro/pub/cs/systems/eim/Colocviul_13/Colocviul_13MainActivity.java
@@ -1,8 +1,10 @@
 package ro.pub.cs.systems.eim.Colocviul_13;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,12 +17,16 @@ public class Colocviul_13MainActivity extends AppCompatActivity {
     private Button south_button;
     private TextView textView;
 
+    private int countButtons;
+
 
     private GenericButtonClickListener genericButtonClickListener = new GenericButtonClickListener();
     private class GenericButtonClickListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
             textView.setAlpha(1);
+            countButtons++;
+            Log.d("filtru", "count value = " + countButtons);
             textView.setText(textView.getText().toString() + ((Button)view).getText().toString() + ", ");
         }
     }
@@ -31,6 +37,13 @@ public class Colocviul_13MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_colocviul_13_main);
 
         textView = (TextView)findViewById(R.id.text_view);
+
+        countButtons = 0;
+        if((savedInstanceState != null) && (savedInstanceState.getInt("count") != 0)) {
+            countButtons = savedInstanceState.getInt("count");
+        }
+
+        Log.d("filtru", "count value = " + countButtons);
 
         north_button = (Button)findViewById(R.id.north_button);
         north_button.setOnClickListener(genericButtonClickListener);
@@ -43,5 +56,22 @@ public class Colocviul_13MainActivity extends AppCompatActivity {
 
         south_button = (Button)findViewById(R.id.south_button);
         south_button.setOnClickListener(genericButtonClickListener);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putInt("count", countButtons);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        countButtons = 0;
+
+        if(savedInstanceState.getString("count") != null ) {
+           countButtons = savedInstanceState.getInt("count");
+        }
     }
 }
